72f7549772649c25248ec33245848fa68f4e11c7
gargdhiren <1234dhirengarg@gmail.com>
2023-05-30 11:33:33 +0530

    start module wrapper

--- Modules/npmMod/index.js
+++ Modules/npmMod/index.js
@@ -1,4 +1,4 @@
 import chalk from "chalk";
 import validator from "validator";
-const res=validator.isEmail("dhirengmail.com");
+const res=validator.isEmail("dhiren@gmail.com");
 console.log(res ? chalk.green.inverse(res):chalk.red.inverse(res));
