f5f02fceb6f0f5c7eef1962c44ac7e77b973ad86
gargdhiren <1234dhirengarg@gmail.com>
2023-06-01 21:53:33 +0530

    manipulation of JSON

--- Modules/JsonNodejs/index.js
+++ Modules/JsonNodejs/index.js
@@ -1,19 +1,20 @@
+const fs= require("fs");
 const bioData={
     name: "Dhiren",
     age : 21,
     college : "thapar university"
 }
 
-console.log(bioData);
+// console.log(bioData);
 
-// this is to convert object to json.
-const jsonData= JSON.stringify(bioData);
-console.log(jsonData);
+// // this is to convert object to json.
+// const jsonData= JSON.stringify(bioData);
+// console.log(jsonData);
 
-//parse json to object
+// //parse json to object
 
-const objectData=JSON.parse(jsonData);
-console.log(objectData);
+// const objectData=JSON.parse(jsonData);
+// console.log(objectData);
 
 
 // 1:convert to jsonData
@@ -21,3 +22,17 @@ console.log(objectData);
 // 3: readFile
 // 4: again convert back to js obj Notation 
 // 5: console.log();
+
+//step 1:
+const jsonData=JSON.stringify(bioData);
+
+//step 2:
+fs.writeFile('json1.json',jsonData,(err)=>{
+    console.log(err);
+});
+
+fs.readFile('json1.json','utf-8',(err,data)=>{
+    //console.log(data);
+    const orgData=JSON.parse(data);
+    console.log(orgData);
+});
