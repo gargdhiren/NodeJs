4d11f479c46d1cf49d382140f3bc89275a22af5b
gargdhiren <1234dhirengarg@gmail.com>
2023-06-01 21:38:00 +0530

    routing on node server

--- Modules/httpserver/index.js
+++ Modules/httpserver/index.js
@@ -4,7 +4,18 @@ const http= require("http");
 const server = http.createServer((req,res)=>{
     //req to request the server for something
     //res is the response after the request
-    res.end("hello from the other side");
+    //console.log(req.url);
+    //we do not need to require the url for using url.
+    if(req.url == '/'){
+        res.end("hello from home");
+    } else if(req.url == '/contact'){
+        res.end("hello from the contact side");
+    }
+    else{
+        //this for tell the server that this url is not present and it is an error.
+        res.writeHead(404);
+        res.end("404 page not found");
+    }
 });
 //to send a request
 //127.0.0.1 is localhost
