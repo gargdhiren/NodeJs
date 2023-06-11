a089f8ae9fb4ebec3afa271f34430db3dcec62f0
gargdhiren <1234dhirengarg@gmail.com>
2023-06-11 11:13:21 +0530

    readStream

---
+++ Modules/StreamModule/index.js
@@ -0,0 +1,33 @@
+const fs=require('fs');
+const http=require('http');
+
+const server=http.createServer();
+
+server.on('request',(req,res)=>{
+    //this is not streaming 
+    //if we have a very long text or large file it will keep on loading as it not runnig simultaneously.
+    // fs.readFile('input.txt',(err,data)=>{
+    //     if(err) console.log(err);
+    //     else res.end(data.toString());
+    // });
+
+    const rstream= fs.createReadStream('input.txt');
+    //data event and only this is not enough.
+    rstream.on('data',(chunkData)=>{
+        res.write(chunkData);
+    })
+    //this to end the rstream event
+    //it is fired when there is no more data to it.
+    rstream.on('end',()=>{
+        res.end();
+    })
+
+    //for any errors
+    rstream.on('error',(err)=>{
+        console.log(err);
+    })
+});
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('server started on port number 8000 on localhost!!!')
+})
